5591774b9c8a130fe5c9586ba0989367c60a0ac0
Diloram Permanova <48168058+DLRMCAN@users.noreply.github.com>
2019-09-11 22:38:40 -0400

    added files

---
+++ output.js
@@ -0,0 +1,12 @@
+function myFortune(){
+    var children = 5;
+    var spouse = "a dentist";
+    var location = "South Korea";
+    var job = "Cook";
+    var mail= document.form.email.value;
+    var name = document.form.name.value;
+    var lastname = document.form.lastname.value;
+
+    document.write ("Hello! " + name + " " + lastname + " you will be a famous " + job + "  and live in " + location + " , will be married to " + spouse + " and have " + children + " kids.");
+
+}
